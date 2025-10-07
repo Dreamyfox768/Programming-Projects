1d2389513724eee92b328b5a1656a97104da496b
Arya <farya@gmu.edu>
2025-10-06 20:32:32 -0400

    Update personal.py

--- AI-resume/personal.py
+++ AI-resume/personal.py
@@ -1,15 +1,26 @@
+
+
 class Personal:
     def __init__(self):
         self.full_name = ""
         self.email = ""
-        self.email_end=""
         self.phone = ""
 
     def collect_info(self):
         self.full_name = input("Enter your full name: ").strip()
-        self.email = input("Enter your personal email: ").strip()
-        self.email_end=input("Enter platform email used: yahoo? gmail?").strip()
+        self.email = input("Enter your personal email_name: ").strip()
+        self.email2 = input("Enter your email provider (gmail,yahoo, outlook ): ").strip()
         self.phone = input("Enter your personal phone number: ").strip()
 
+
     def __str__(self):
-        return f"{self.full_name} | Email: {self.email}@{self.email_end}.com | Phone: {self.phone}"
+        return f"{self.full_name} | Email: {self.email}@{self.email2}.com| Phone: {self.phone}"
+
+def main():
+    print("\n=== Top of the resume: personal information ===")
+    user = Personal()
+    user.collect_info()
+    print(user)
+
+if __name__ == "__main__":
+    main()
