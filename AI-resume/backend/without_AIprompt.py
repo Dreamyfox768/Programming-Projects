13fe1f5b0918c42fca0a2f5428fdb40088338037
Dreamyfox <farya@gmu.edu>
2025-10-08 21:57:16 -0400

    Update without_AIprompt.py

--- AI-resume/backend/without_AIprompt.py
+++ AI-resume/backend/without_AIprompt.py
@@ -16,9 +16,7 @@ class body_part2:
         #language
         #tools
         #operating system
-        #application
-
-
+        #application 
         ...
 
     def certs(self):
@@ -27,11 +25,7 @@ class body_part2:
             self.num -= 1
             self.start = input("Enter start date for certs received:").strip()
             self.company = input("Enter company name").strip()
-            self.num = int(input("enter how many job exprience you have, Enter integer:  enter zero to move on  "))
-
-
-
-
+            self.num = int(input("enter how many job exprience you have, Enter integer:  enter zero to move on  ")
 
     def __str__(self):
         ...
