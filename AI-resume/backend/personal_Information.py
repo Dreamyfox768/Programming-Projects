cb9ba9b70eebe37197f5cc186932f013237e88d9
Dreamyfox <farya@gmu.edu>
2025-10-09 20:56:28 -0400

    Update personal_Information.py

--- AI-resume/backend/personal_Information.py
+++ AI-resume/backend/personal_Information.py
@@ -5,21 +5,37 @@ class Personal:
         self.full_name = ""
         self.email = ""
         self.phone = ""
+        self.email2= " "
+        self.start = ''
+        self.end = ''
+        self.major = ''
+        self.school = ''
 
     def collect_info(self):
         self.full_name = input("Enter your full name: ").strip()
         self.email = input("Enter your personal email_name: ").strip()
         self.email2 = input("Enter your email provider (gmail,yahoo, outlook ): ").strip()
         self.phone = input("Enter your personal phone number: ").strip()
+    def education(self):
+        self.major = input("Enter your major: ").strip()
+        self.start = input("Enter start date your major:").strip()
+        self.end = input("end your major: ").strip()
+        self.school = input("School attending: ").strip()
+
+
 
 
     def __str__(self):
-        return f"{self.full_name} | Email: {self.email}@{self.email2}.com| Phone: {self.phone}"
+        return (f"{self.full_name}\n "
+                f" | Email: {self.email}@{self.email2}.com| Phone: {self.phone}\n"
+                f"Education:  {self.major} | {self.school}                       date:{self.start}-{self.end}")
 
 def main():
-    print("\n=== Top of the resume: personal information ===")
+    print("\n=== Top of resume: personal information ===")
     user = Personal()
     user.collect_info()
+    print("\n=== Top of resume: Education information ===")
+    user.education()
     print(user)
 
 if __name__ == "__main__":
