b2edb617a5660ea794b49ab2a88b935f08a16d2f
Dreamyfox <farya@gmu.edu>
2025-10-10 18:31:28 -0400

    Create backend.py

---
+++ Reading_Corner/backend/backend.py
@@ -0,0 +1,18 @@
+class Books:
+    def __init__(self):
+        self.author = ''
+        self.bookname = ''
+        self.isbn = ''
+        self.first = ''
+        self.last = ''
+        self.full_name = ''
+
+    def author(self ):
+        self.full_name = self.first + self.last
+
+
+def main():
+    ...
+
+if __name__ == "-__main__":
+    main()
