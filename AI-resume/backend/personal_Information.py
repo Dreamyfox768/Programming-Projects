235cbad1bfc16224a8aa8f7b4dce186d4cdb48ae
Dreamyfox <farya@gmu.edu>
2025-10-22 22:17:39 -0400

    Refactor collect_info and education methods
    Updated collect_info and education methods to accept UI data as arguments. Added checks to use provided data if available, otherwise prompts for user input.

--- AI-resume/backend/personal_Information.py
+++ AI-resume/backend/personal_Information.py
@@ -11,24 +11,34 @@ class Personal:
         self.major = ''
         self.school = ''
 
-    def collect_info(self):
+    def collect_info(self, *ui_data): #todo ui merging
+        if ui_data: #todo make sure to add this for alllllllll
+            self.full_name = ui_data[0]
+            self.email = ui_data[1]
+            self.phone = ui_data[2]
+            return
         self.full_name = input("Enter your full name: ").strip()
         self.email = input("Enter your personal email_name: ").strip()
         self.email2 = input("Enter your email provider (gmail,yahoo, outlook ): ").strip()
         self.phone = input("Enter your personal phone number: ").strip()
-    def education(self):
+
+    def education(self, *ui_data):
+        if ui_data:
+            self.major = ui_data[0]
+            self.start = ui_data[1]
+            self.end = ui_data[2]
+            self.school = ui_data[3]
+
         self.major = input("Enter your major: ").strip()
         self.start = input("Enter start date your major:").strip()
         self.end = input("end your major: ").strip()
         self.school = input("School attending: ").strip()
 
-
-
-
     def __str__(self):
         return (f"{self.full_name}\n "
                 f" | Email: {self.email}@{self.email2}.com| Phone: {self.phone}\n"
                 f"Education:  {self.major} | {self.school}                       date:{self.start}-{self.end}")
+
 
 def main():
     print("\n=== Top of resume: personal information ===")
